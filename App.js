02e4a4fa6fe0b8b340907d0d969e239a615a97db
Anant Chaturvedi <98809611+anant23888@users.noreply.github.com>
2023-07-21 17:16:58 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
 import './App.css';
-import Home from "./Home";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter,createRoutesFromElements,Route,RouterProvider} from 'react-router-dom';
+import Product from './components/Product';
+import Dashboard from './components/Dashboard';
+import { Cart } from './components/Cart';
+import RootLayout from './components/RootLayout';
 function App() {
+  const router=createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Dashboard/>}></Route>
+      <Route path='/cart' element={<Cart/>}></Route>
+      {/* <Route path='/product' element={<C/>}></Route> */}
+    </Route>
+  ))
   return (
     <div className="App">
-     <Home />
+      <RouterProvider router={router} />
     </div>
   );
 }
